bfa5da5528054f68e12b0a1389e58bc6c7f8b0fe
preet_jaisinghani <jayakukreja18@gmail.com>
2022-12-21 14:46:48 +0530

    responsiveness

--- lib/play_quiz_screen.dart
+++ lib/play_quiz_screen.dart
@@ -44,61 +44,63 @@ class _PlayQuizScreenState extends State<PlayQuizScreen> {
 
           QuestionAnswerModel model = quizQuestions[index];
           // isAnswerLocked = false;
-          return Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Text(
-                      model.question,
-                      style: const TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Column(
-                        children: List.generate(
-                      model.options.length,
-                      (index) => InkWell(
-                        onTap: () {
-                          setState(() {
-                            isAnswerLocked = true;
-                            selectedAnswer = model.options[index];
-                            correctAnswer = model.correctAnswer;
-                          });
-                          print("isanswerlocked: $isAnswerLocked");
-                        },
-                        child: Container(
-                          width: double.infinity,
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 30),
-                          margin: const EdgeInsets.only(bottom: 10),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: foregroundColor),
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(7)),
-                              color: selectedAnswer == model.options[index]
-                                  ? foregroundColor
-                                  : backgroundColor),
-                          child: Text(
-                            model.options[index],
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      Text(
+                        model.question,
+                        style: const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Column(
+                          children: List.generate(
+                        model.options.length,
+                        (index) => InkWell(
+                          onTap: () {
+                            setState(() {
+                              isAnswerLocked = true;
+                              selectedAnswer = model.options[index];
+                              correctAnswer = model.correctAnswer;
+                            });
+                            print("isanswerlocked: $isAnswerLocked");
+                          },
+                          child: Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 30),
+                            margin: const EdgeInsets.only(bottom: 10),
+                            decoration: BoxDecoration(
+                                border: Border.all(color: foregroundColor),
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(7)),
+                                color: selectedAnswer == model.options[index]
+                                    ? foregroundColor
+                                    : backgroundColor),
+                            child: Text(
+                              model.options[index],
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ))
-                  ],
+                      ))
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           );
         },
       ),
